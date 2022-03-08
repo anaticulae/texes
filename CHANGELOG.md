f34c730b8fa1a03cee143452f95be966970a7b8b
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-03-08 14:54:37 +0100

    0.8.1 auto generated release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 Every noteable change is logged here.
 
+## v0.8.1
+
 ## v0.8.0
 
 ### Feature
--- texas/__init__.py
+++ texas/__init__.py
@@ -9,7 +9,7 @@
 
 import os
 
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 PACKAGE = 'texas'
