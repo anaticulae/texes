af485163ba639dd16ac54827a442f2d56adc7b84
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-06-19 13:26:41 +0200

    test(docref): validate section references

---
+++ tests/docref_/section/test_validate.py
@@ -0,0 +1,79 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import functools
+
+import power
+import pytest
+import serializeraw
+import utila
+import utilatest
+
+import docref
+import docref.path
+import tests.conftest
+import tests.docref_
+
+ARCHIVE = utila.join(
+    docref.ROOT,
+    'tests/docref_/section/expected',
+    exist=True,
+)
+
+RESOURCES = [
+    pytest.param(
+        power.pdf(source),
+        power.ctext(power.pdf(source), default=':'),
+        id=utila.file_name(power.pdf(source)),
+    ) for source in tests.conftest.RESOURCES
+]
+
+
+@utilatest.nightly
+@pytest.mark.parametrize('source, pages', RESOURCES)
+def test_validate_sectionref(source, pages, testdir, monkeypatch):
+    utilatest.fixture_requires(source)
+    Evaluate(
+        source=source,
+        pages=pages,
+        workdir=testdir.tmpdir,
+        monkeypatch=monkeypatch,
+    ).evaluate()
+
+
+class Evaluate(utilatest.BaseLiner):
+
+    def __init__(self, source, pages, workdir, monkeypatch):
+        super().__init__(
+            program=functools.partial(
+                tests.docref_.run,
+                monkeypatch=monkeypatch,
+            ),
+            step='section',
+            pages=pages,
+            source=power.link(source),
+            workdir=workdir,
+            archive=ARCHIVE,
+            loader=self.frompath,
+            convert_source=False,
+        )
+        self.headlines = power.link(source)
+
+    def frompath(self, path):  # pylint:disable=R0201
+        path = docref.path.docref_section(path)
+        references = serializeraw.load_docref(path)
+        return references
+
+    def raw(self, value) -> str:
+        collected = [
+            f'{item.page} {item.sentence} {"   ".join(item.raw)}'
+            for item in value
+        ]
+        result = utila.NEWLINE.join(collected)
+        return result
