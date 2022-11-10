80c6b90592ab313de0f02f99614e76d73cd556d8
Helmut Konrad Fahrendholz <helmutus@outlook.com>
2022-11-10 22:03:35 +0000

    chore(requirements): upgrade requirements.txt
    + add regression test that hyperlink parser parses not multiple
    + adjust baseline

--- tests/weblink_/test_regression.py
+++ tests/weblink_/test_regression.py
@@ -1,19 +1,20 @@
 # =============================================================================
 # C O P Y R I G H T
 # -----------------------------------------------------------------------------
-# Copyright (c) 2021-2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# Copyright (c) 2022 by Helmut Konrad Fahrendholz. All rights reserved.
 # This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-utila>=2.93.0<3.0.0
-iamraw>=4.88.5<5.0.0
-# split sentences and determine language of written sentence
-german>=1.27.1<2.0.0
+import weblink.features.sentence
 
-words>=1.23.0<2.0.0
-configo>=0.25.0<1.0.0
 
-# parse bib tech labels [FHK2016]
-bibliography>=0.3.0<1.0.0
+def test_try_merge():
+    raw = 'unter: file:///C:/Users/user/Downloads/MEMO-16-2265_DE.pdf'
+    merged = weblink.features.sentence.try_merge(raw)
+    assert len(merged) == 1
+    merged = weblink.features.sentence.try_merge(raw)
+    assert len(merged) == 1
+    merged = weblink.features.sentence.try_merge(raw)
+    assert len(merged) == 1
